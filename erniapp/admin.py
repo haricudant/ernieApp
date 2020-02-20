c6ffd133167ff359eaed6dbd62f649ae3dfaeb5d
Hari Narayanan <hnarayanan5@gmail.com>
2020-02-20 17:27:15 +0000

    first commit

---
+++ erniapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from erniapp.models import  User, Userdetails
+# Register your models here.
+admin.site.register(Userdetails)
